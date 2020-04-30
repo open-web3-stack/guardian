42905b54c8a9ab22803f4c880ef862fa1edca3a6
Ermal Kaleci <ermalkaleci@gmail.com>
2020-04-30 13:19:23 +0200

    implement config template

--- bin/cli.spec.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-
-describe('cli', () => {
-  it('yaml load works', () => {
-    const config = yaml.safeLoad(fs.readFileSync('./src/__tests__/config.yaml', 'utf8'));
-    expect(config).toMatchObject({ version: '0.1' });
-  });
-});
---
+++ bin/read-config.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
+import { template } from 'lodash';
+
+const regex = /\#[^*]*?$/gm;
+
+export default (path: string) => {
+  const data = fs.readFileSync(path, 'utf8');
+
+  // remove all comments
+  const configStr = data.replace(regex, '');
+
+  // replace variables
+  const config = template(configStr)(process.env);
+
+  return yaml.safeLoad(config);
+};
