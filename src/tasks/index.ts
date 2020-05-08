ff75f0d92b12f26bdb644f64cc12b2ba277f595c
Ermal Kaleci <ermalkaleci@gmail.com>
2020-05-08 18:17:55 +0200

    generic substrate guardian (#19)

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -5,6 +5,7 @@ import { Observable } from '@polkadot/types/types';
 import ethereum from './ethereum';
 import laminarChain from './laminarChain';
 import acalaChain from './acalaChain';
+import substrate from './substrate';
 
 export const createEthereumTasks = (api$: Observable<EthereumApi>) => ({
   ...ethereum(api$),
@@ -16,4 +17,8 @@ export const createLaminarTasks = (api$: Observable<LaminarApi>) => ({
 
 export const createAcalaTasks = (api$: Observable<ApiRx>) => ({
   ...acalaChain(api$),
+});
+
+export const createSubstrateTasks = (api$: Observable<ApiRx>) => ({
+  ...substrate(api$),
 });
--- src/tasks/laminarChain/index.ts
+++ src/tasks/laminarChain/index.ts
@@ -2,40 +2,27 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { LaminarApi } from '@laminar/api';
 
+import substrate from '../substrate';
 import createLaminarApi from './createLaminarApi';
-
 import TraderInfoTask from './TraderInfoTask';
 import PositionsByTraderTask from './PositionsByTraderTask';
 import PoolInfoTask from './PoolInfoTask';
-
-import EventsTask from '../shared/EventsTask';
-import StorageTask from '../shared/StorageTask';
-import AccountsTask from '../shared/AccountsTask';
-
 import LiquidityPoolTask from './LiquidityPoolTask';
-
 import BalancesTask from './BalancesTask';
 
-export default (api$: Observable<LaminarApi>) => {
-  const apiRx$ = api$.pipe(map(({ api }) => api));
-  return {
-    system: {
-      events: new EventsTask(apiRx$),
-      storage: new StorageTask(apiRx$),
-      accounts: new AccountsTask(apiRx$),
-    },
-    synthetic: {
-      liquidityPool: new LiquidityPoolTask(api$),
-    },
-    margin: {
-      traderInfo: new TraderInfoTask(api$),
-      positionsByTrader: new PositionsByTraderTask(api$),
-      poolInfo: new PoolInfoTask(api$),
-    },
-    account: {
-      balances: new BalancesTask(api$),
-    },
-  };
-};
-
 export { createLaminarApi };
+
+export default (api$: Observable<LaminarApi>) => ({
+  ...substrate(api$.pipe(map(({ api }) => api))),
+  synthetic: {
+    liquidityPool: new LiquidityPoolTask(api$),
+  },
+  margin: {
+    traderInfo: new TraderInfoTask(api$),
+    positionsByTrader: new PositionsByTraderTask(api$),
+    poolInfo: new PoolInfoTask(api$),
+  },
+  account: {
+    balances: new BalancesTask(api$),
+  },
+});
