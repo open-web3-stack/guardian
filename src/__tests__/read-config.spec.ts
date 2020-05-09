3628b1f960b7c1cd9e165a52645c5a5bb048284b
Ermal Kaleci <ermalkaleci@gmail.com>
2020-05-09 14:54:02 +0200

    setup build & publish (#21)

--- src/__tests__/read-config.spec.ts
+++ src/__tests__/read-config.spec.ts
@@ -3,7 +3,7 @@ import readConfig from '../read-config';
 describe('read-config', () => {
   it('works', () => {
     process.env = { a: '1' };
-    const config = readConfig('./bin/__tests__/test.yaml');
+    const config = readConfig('./src/__tests__/test.yaml');
     expect(config).toMatchObject({ foo: 1 });
   });
 });
