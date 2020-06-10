d67c8801ab39df6c7b44305e37e7e100ed4aa085
Ermal Kaleci <ermalkaleci@gmail.com>
2020-06-11 00:27:33 +0200

    cleanup

--- examples/collateral_auction/test.ts
+++
@@ -1,7 +0,0 @@
-process.env.MARGIN = '0.05';
-process.env.BIDDER_SURI = '//Charlie';
-process.env.BIDDER_ADDRESS = '5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y';
-process.env.NODE_ENDPOINT = 'ws://localhost:9944'; //'wss://testnet-node-1.acala.laminar.one/ws';
-
-import run from '.';
-run();
