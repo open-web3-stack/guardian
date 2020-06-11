d389395eb7b33eed6bb231f8d269db3d06f4bd25
aniiantt <nsso@foxmail.com>
2020-06-11 16:23:41 +1200

    fix config

--- examples/liquidation_bot/generateConfig.ts
+++ examples/liquidation_bot/generateConfig.ts
@@ -8,13 +8,13 @@ const getConfig = async () => {
         name: 'syntheticPoolGeneral',
         id: '0x61A7645ef693Ea7740b121232Ddc7b874Be7Fa09',
         tokenNames: ['FEUR', 'FJPY', 'FCAD', 'FCHF', 'FGBP', 'FAUD', 'FOIL', 'FXAU', 'FBTC', 'FETH'],
-        maxCollateralRatio: 0.8,
+        maxCollateralRatio: 1.04,
       },
       {
         name: 'syntheticPoolXYZ',
         id: '0x2b1627CFF4Eb38C774Ec1008045CEd2C4776eEf6',
         tokenNames: ['FEUR', 'FJPY', 'FCAD', 'FCHF', 'FGBP', 'FAUD', 'FOIL', 'FXAU', 'FBTC', 'FETH'],
-        maxCollateralRatio: 0.8,
+        maxCollateralRatio: 1.04,
       },
     ],
   };
