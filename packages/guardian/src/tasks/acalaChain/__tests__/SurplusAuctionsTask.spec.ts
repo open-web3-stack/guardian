e1e86d463b1be4cf18eda25abb8e278965a09f02
Ermal Kaleci <ermalkaleci@gmail.com>
2020-06-18 00:24:51 +0200

    update tests

--- packages/guardian/src/tasks/acalaChain/__tests__/SurplusAuctionsTask.spec.ts
+++ packages/guardian/src/tasks/acalaChain/__tests__/SurplusAuctionsTask.spec.ts
@@ -8,7 +8,14 @@ describe('SurplusAuctionsTask', () => {
 
   it('works with mock', (done) => {
     new SurplusAuctionsTask(api$).run(null).subscribe((result) => {
-      expect(result).toStrictEqual({ auctionId: 0, amount: '100', startTime: 1, endTime: 125 });
+      expect(result).toStrictEqual({
+        auctionId: 0,
+        amount: '100',
+        startTime: 1,
+        endTime: 125,
+        lastBid: '30',
+        lastBidder: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+      });
       done();
     });
   });
