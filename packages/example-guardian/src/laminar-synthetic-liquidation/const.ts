7309a11ae176bc2e58436643c7cfa8f44486688e
Ermal Kaleci <ermalkaleci@gmail.com>
2020-08-07 01:21:53 +0200

    hotfix config path

--- packages/example-guardian/src/laminar-synthetic-liquidation/const.ts
+++ packages/example-guardian/src/laminar-synthetic-liquidation/const.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export const readConst = () => {
   if (!process.argv.find((i) => i.startsWith('--config'))) {
     process.argv.push('--config');
-    process.argv.push(path.resolve(__dirname, 'laminar-synthetic-liquidation-guardian.yml'));
+    process.argv.push(path.resolve(__dirname, '..', 'laminar-synthetic-liquidation-guardian.yml'));
   }
 
   const nodeEndpoint = getEnv('NODE_ENDPOINT');
