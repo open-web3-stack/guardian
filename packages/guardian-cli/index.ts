c864363d6f9304acb02b120bc78013f5cf0cd079
Ermal Kaleci <ermalkaleci@gmail.com>
2022-07-08 14:18:21 +0200

    fix import

--- packages/guardian-cli/index.ts
+++ packages/guardian-cli/index.ts
@@ -1,5 +1,2 @@
-import '@open-web3/acala-guardian';
-import '@open-web3/laminar-guardian';
 import './src/cli';
-import readConfig from './src/read-config';
-export { readConfig };
+export * from './src/cli';
